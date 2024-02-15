ebcaac8243674ea8312c539c232a0fbb2a8491eb
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-02-15 11:06:59 -0300

    :sparkles: feat: add drum sounds

--- index.js
+++ index.js
@@ -17,39 +17,38 @@ function playSound(event) {
 
     switch(choice) {
         case "w":
-            //var wAudio = new Audio();
-            //wAudio.play();
-            alert("You've clicked W!");
+            var wAudio = new Audio("sounds/tom-1.mp3");
+            wAudio.play();
         break;
 
         case "a":
-            //var aAudio = new Audio();
-            //aAudio.play();
-            alert("You've clicked A!"); 
+            var aAudio = new Audio("sounds/tom-2.mp3");
+            aAudio.play();
+        break;
+
+        case "s":
+            var sAudio = new Audio("sounds/tom-3.mp3");
+            sAudio.play();
         break;
 
         case "d":
-            //var dAudio = new Audio();
-            //dAudio.play();
-            alert("You've clicked D!");
+            var dAudio = new Audio("sounds/tom-4.mp3");
+            dAudio.play();
         break;
 
         case "j":
-            //var jAudio = new Audio();
-            //jAudio.play();
-            alert("You've clicked J!");
+            var jAudio = new Audio("sounds/snare.mp3");
+            jAudio.play();
         break;
 
         case "k":
-            //var kAudio = new Audio();
-            //kAudio.play();
-            alert("You've clicked K!");
+            var kAudio = new Audio("sounds/crash.mp3");
+            kAudio.play();
         break;
 
         case "l":
-            //var lAudio = new Audio();
-            //lAudio.play();
-            alert("You've clicked L!");
+            var lAudio = new Audio("sounds/kick-bass.mp3");
+            lAudio.play();
         break;
 
         default:
